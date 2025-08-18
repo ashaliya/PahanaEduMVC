e2f30bb29b4f597d364d3cb9d4671dcf79c7e2dd
ashaliya <ashaliyaamarasinghe17@gmail.com>
2025-08-19 01:59:55 +0530

    Updated Customer model with new fields.

--- src/java/model/Customer.java
+++ src/java/model/Customer.java
@@ -4,15 +4,24 @@ public class Customer {
     private String name;
     private String address;
     private String phone;
+    private String email;
     private int unitsConsumed;
+    
     public int getAccountNumber(){ return accountNumber; }
     public void setAccountNumber(int accountNumber){ this.accountNumber = accountNumber; }
+    
     public String getName(){ return name; }
     public void setName(String name){ this.name = name; }
+  
     public String getAddress(){ return address; }
     public void setAddress(String address){ this.address = address; }
+    
     public String getPhone(){ return phone; }
     public void setPhone(String phone){ this.phone = phone; }
+    
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    
     public int getUnitsConsumed(){ return unitsConsumed; }
     public void setUnitsConsumed(int unitsConsumed){ this.unitsConsumed = unitsConsumed; }
 }
